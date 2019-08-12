7a3d71126f69852436cbb368f7293765a82d53d2
Christopher A. Mosher <cmosher01@gmail.com>
2019-08-11 23:45:39 -0400

    simplfy generation algorithm; update gradle

--- src/main/java/nu/mine/mosher/gedcom/GedcomUidGenerator.java
+++ src/main/java/nu/mine/mosher/gedcom/GedcomUidGenerator.java
@@ -1,16 +1,12 @@
 package nu.mine.mosher.gedcom;
 
-import com.google.common.io.BaseEncoding;
-
-import java.nio.ByteBuffer;
-import java.util.Base64;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.security.SecureRandom;
+import java.util.*;
 
 public class GedcomUidGenerator {
     private final int length;
     private final Set<String> generated = new HashSet<>(1024);
+    private final Random rnd = new SecureRandom();
 
     public GedcomUidGenerator(final int length) {
         this.length = length;
@@ -23,7 +19,7 @@ public class GedcomUidGenerator {
     public String generateId() {
         int sanity = 100000;
         String s = generateCandidateIdOfLength();
-        while (sanity > 0 && (s.contains("_") || s.contains("-") || startsWithDigit(s) || this.generated.contains(s))) {
+        while (sanity > 0 && this.generated.contains(s)) {
             --sanity;
             s = generateCandidateIdOfLength();
         }
@@ -34,24 +30,15 @@ public class GedcomUidGenerator {
         return s;
     }
 
-    private static boolean startsWithDigit(String s) {
-        final char x = s.charAt(0);
-        return '0' <= x && x <= '9';
-    }
 
-    private String generateCandidateIdOfLength() {
-        return generateCandidateId().substring(0, this.length);
-    }
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    private static String generateCandidateId() {
-        return base64encode(rbFromUuid(UUID.randomUUID()));
-    }
+    public String generateCandidateIdOfLength() {
+        final StringBuilder sb = new StringBuilder(this.length);
+        for (int i = 0; i < this.length; ++i) {
+            sb.append(ALPHABET.charAt(this.rnd.nextInt(ALPHABET.length())));
+        }
+        return sb.toString();
 
-    private static byte[] rbFromUuid(final UUID uuid) {
-        return ByteBuffer.allocate(16).putLong(uuid.getMostSignificantBits()).putLong(uuid.getLeastSignificantBits()).array();
-    }
-
-    private static String base64encode(final byte[] rb) {
-        return BaseEncoding.base64Url().encode(rb);
     }
 }
